6b742d093b9d74ebca9baf822f5e97f74daf2245
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-02-01 17:15:50 +0700

    Add files via upload

---
+++ Lab-01/Exercise6/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div>
+    <?php
+        $image1 = "images1.png";
+        echo "<center><img src=$image1 width=300 height=400>";
+    ?>
+    </div>
+</body>
+</html>
