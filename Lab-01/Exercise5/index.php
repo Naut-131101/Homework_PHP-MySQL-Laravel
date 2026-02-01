fcf9aebd8ee24c0651d1c022bb27627cd8f021df
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-02-01 17:14:20 +0700

    Add files via upload

---
+++ Lab-01/Exercise5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+        $title = "This heading is in the center";
+        $txt1 = "This is paragraph one and should be on top";
+        $txt2 = "This is paragraph two and should be on bottom";
+        $txt3 = "You delivered your assignment ontime.";
+
+        echo "<h1><center> $title </h1>";
+        echo "<p> $txt1 <hr></p>";
+        echo "<p> Hello <br> $txt3 <br> Thanks <br> Mahnaz <hr></p>";
+        echo "<p> $txt2 </p>";
+    ?>
+</body>
+</html>
