a62355fa734bb2e85e73eee3960751399a3680ea
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-02-01 17:16:45 +0700

    Add files via upload

---
+++ Lab-03/welcome.php
@@ -0,0 +1,11 @@
+<html>
+    <style>
+        table, th, td {
+            border:1px solid black;
+        }
+    </style>
+<body>
+    <h1>Welcome <?php echo $_POST["name"]; ?></h1>
+    <p>Chúng tôi luôn chào đón bạn!!! Newbie</p>
+</body>
+</html>
